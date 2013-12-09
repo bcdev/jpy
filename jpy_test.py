13c40abb0c849f5a77215fa73ccf024cb4032e30
forman <norman.fomferra@brockmann-consult.de>
2013-12-09 22:00:38 +0100

    Added some basic tests

--- jpy_test.py
+++ jpy_test.py
@@ -1,18 +1,73 @@
+import unittest
 import jpy
 
-jpy.create_jvm(options=['-Xmx512M'], debug=True)
+jpy.create_jvm(options=['-Xmx512M'])
 
-S = jpy.get_class('java.lang.String')
-s = S('Hello JPy!')
+class TestString(unittest.TestCase):
 
-print('S =', S)
-print('str(S) =', str(S))
-print('s =', s)
-print('str(s) =', str(s))
+    def setUp(self):
+        self.String = jpy.get_jtype('java.lang.String')
+        self.assertTrue('java.lang.String' in jpy.jtypes)
 
-s = s.substring(0, 5)
+    def test_constructor(self):
+        s = self.String('Bibo')
+        self.assertEqual(type(s), self.String)
+        self.assertEqual(str(s), 'Bibo')
 
-print('s =', s)
-print('str(s) =', str(s))
+    def test_toString(self):
+        s = self.String('Bibo')
+        self.assertTrue('toString' in self.String.__dict__)
+        s = s.toString()
+        self.assertEqual(s, 'Bibo')
 
-jpy.destroy_jvm()
+    def test_substring(self):
+        s = self.String('Bibo')
+        self.assertTrue('substring' in self.String.__dict__)
+        s2 = s.substring(0, 2)
+        self.assertEqual(s2, 'Bi')
+        s2 = s.substring(2)
+        self.assertEqual(s2, 'bo')
+
+
+import os
+
+class TestFile(unittest.TestCase):
+
+    def setUp(self):
+        self.File = jpy.get_jtype('java.io.File')
+        self.assertTrue('java.io.File' in jpy.jtypes)
+
+    def test_constructor(self):
+        f = self.File('/usr/local/bibo')
+        self.assertEqual(type(f), self.File)
+        self.assertEqual(str(f).split(os.sep), ['', 'usr', 'local', 'bibo'])
+
+    def test_getPath(self):
+        f = self.File('/usr/local/bibo')
+        self.assertTrue('getPath' in self.File.__dict__)
+        path = f.getPath()
+        self.assertEqual(path.split(os.sep), ['', 'usr', 'local', 'bibo'])
+
+    def test_getName(self):
+        f = self.File('/usr/local/bibo')
+        self.assertTrue('getName' in self.File.__dict__)
+        name = f.getName()
+        self.assertEqual(name, 'bibo')
+
+    def test_toPath(self):
+        f = self.File('/usr/local/bibo')
+        self.assertTrue('toPath' in self.File.__dict__)
+        path = f.toPath()
+        self.assertEqual(str(type(path)), '<class \'java.nio.file.Path\'>')
+
+        jpy.get_jtype('java.nio.file.Path')
+        n1 = path.getName(0)
+        n2 = path.getName(1)
+        n3 = path.getName(2)
+        self.assertEqual(str(n1), 'usr')
+        self.assertEqual(str(n2), 'local')
+        self.assertEqual(str(n3), 'bibo')
+
+
+if __name__ == '__main__':
+    unittest.main()
