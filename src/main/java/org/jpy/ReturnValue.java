0191cdb9e3b5a30eeb0be7c7edc866218e977816
forman <norman.fomferra@brockmann-consult.de>
2013-12-10 09:47:04 +0100

    Added annotations for method parameters

---
+++ src/main/java/org/jpy/ReturnValue.java
@@ -0,0 +1,14 @@
+package org.jpy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Norman Fomferra
+ */
+@Target(value = ElementType.PARAMETER)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface ReturnValue {
+}
