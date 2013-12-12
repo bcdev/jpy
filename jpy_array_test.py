bba05160374a4229864f3acef423a697333827be
forman <norman.fomferra@brockmann-consult.de>
2013-12-12 12:48:16 +0100

    Renamings

--- jpy_array_test.py
+++ jpy_array_test.py
@@ -7,7 +7,7 @@ jpy.create_jvm(options=['-Djava.class.path=target/test-classes', '-Xmx512M'], de
 
 class TestMethodReturnValues(unittest.TestCase):
 
-    def do_test_array(self, type, initial, expected):
+    def do_array_get_set(self, type, initial, expected):
         a = jpy.array(type, 3)
         self.assertEqual(len(a), 3)
         self.assertEqual(a[0], initial[0])
@@ -20,7 +20,7 @@ class TestMethodReturnValues(unittest.TestCase):
         self.assertEqual(a[1], expected[1])
         self.assertEqual(a[2], expected[2])
 
-    def do_test_float_array(self, initial, expected):
+    def do_float_array_get_set(self, initial, expected):
         a = jpy.array('float', 3)
         self.assertEqual(len(a), 3)
         self.assertEqual(a[0], initial[0])
@@ -35,11 +35,11 @@ class TestMethodReturnValues(unittest.TestCase):
 
 
     def test_array_boolean(self):
-        self.do_test_array('boolean', [False, False, False], [True, False, True])
+        self.do_array_get_set('boolean', [False, False, False], [True, False, True])
 
 
     def test_array_byte(self):
-        self.do_test_array('byte', [0, 0, 0], [-128, 100, 127])
+        self.do_array_get_set('byte', [0, 0, 0], [-128, 100, 127])
 
 
     def test_array_char(self):
@@ -48,28 +48,30 @@ class TestMethodReturnValues(unittest.TestCase):
         pass
 
     def test_array_short(self):
-        self.do_test_array('short', [0, 0, 0], [-32768, 100, 32767])
+        self.do_array_get_set('short', [0, 0, 0], [-32768, 100, 32767])
 
 
     def test_array_int(self):
-        self.do_test_array('int', [0, 0, 0], [-2147483648, 100, 2147483647])
+        self.do_array_get_set('int', [0, 0, 0], [-2147483648, 100, 2147483647])
 
 
     def test_array_long(self):
-        self.do_test_array('long', [0, 0, 0], [-9223372036854775808, 100, 9223372036854775807])
+        self.do_array_get_set('long', [0, 0, 0], [-9223372036854775808, 100, 9223372036854775807])
 
 
     def test_array_float(self):
-        self.do_test_float_array([0, 0, 0], [-1.001, 0.001, 1.001])
+        self.do_float_array_get_set([0, 0, 0], [-1.001, 0.001, 1.001])
 
 
     def test_array_double(self):
-        self.do_test_array('double', [0, 0, 0], [-1.001, 0.001, 1.001])
+        self.do_array_get_set('double', [0, 0, 0], [-1.001, 0.001, 1.001])
+
 
     def test_array_object(self):
-        self.do_test_array('java.lang.String', [None, None, None], ['A', 'B', 'C'])
+        self.do_array_get_set('java.lang.String', [None, None, None], ['A', 'B', 'C'])
         F = jpy.get_class('java.io.File')
-        self.do_test_array('java.io.File', [None, None, None], [F('A'), F('B'), F('C')])
+        self.do_array_get_set('java.io.File', [None, None, None], [F('A'), F('B'), F('C')])
+
 
 if __name__ == '__main__':
     unittest.main()
