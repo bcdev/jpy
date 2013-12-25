9cfbdc2c6a25f61388d47749c1a832995ee9e9cc
forman <norman.fomferra@brockmann-consult.de>
2013-12-25 21:14:30 +0100

    Created a dedicated beampy subdirectory

---
+++ beampy/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='beampy',
+      version='0.1',
+      description='BEAM Python API',
+      author='Norman Fomferra, Brockmann Consult GmbH',
+      py_modules=['beampy'],
+      )
+
+import sys
+if sys.argv[1] == 'install':
+    import os
+    os.system(sys.executable + ' beampy_test.py')
