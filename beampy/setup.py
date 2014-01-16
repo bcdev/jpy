77113aaf1cf1af491ae935d37b6a01e95e286c39
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-01-16 20:08:07 +0100

    version='0.4'

--- beampy/setup.py
+++ beampy/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='beampy',
-      version='0.3',
+      version='0.4',
       description='BEAM Python API',
       author='Norman Fomferra, Brockmann Consult GmbH',
       py_modules=['beampy'],
