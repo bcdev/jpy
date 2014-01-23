1de76881faf89289c8c683f63a87931605c005df
forman <norman.fomferra@brockmann-consult.de>
2014-01-23 11:36:53 +0100

    Update

--- src/test/java/org/jpy/UseCases.java
+++ src/test/java/org/jpy/UseCases.java
@@ -6,8 +6,9 @@ import org.junit.Test;
  * @author Norman Fomferra
  */
 public class UseCases {
+
     @Test
-    public void testIt() {
+    public void modifyPythonSysPath() {
         PyLib.startPython();
 
         PyModule builtins = PyModule.importModule("builtins");
@@ -22,12 +23,27 @@ public class UseCases {
 
         String[] objectArrayValue = path.getObjectArrayValue(String.class);
 
-        for (int i = 0; i < objectArrayValue.length; i++) {
-            String s = objectArrayValue[i];
+        for (String s : objectArrayValue) {
             System.out.println("s = " + s);
         }
 
         System.out.println("intValue = " + intValue);
         //System.out.println("value = " + value);
+
+        //PyLib.stopPython();
+    }
+
+    @Test
+    public void executeScriptAsConfig() throws Exception {
+        PyLib.startPython();
+        PyModule module = PyModule.importModule("__main__");
+        System.out.println("module = " + module);
+        PyLib.execScript("a = 1234");
+        PyObject a = module.getAttribute("a");
+        System.out.println("a = " + a);
+        int intValue = a.getIntValue();
+        System.out.println("intValue = " + intValue);
+
+
     }
 }
