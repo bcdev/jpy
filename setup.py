03887a1f01551b4e7c14c171b5d5c0a670555f10
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-06-30 18:05:08 +0200

    --> 0.7.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 __author__ = "Norman Fomferra, Brockmann Consult GmbH"
 __copyright__ = "Copyright (C) 2014 Brockmann Consult GmbH"
 __license__ = "GPL v3"
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 sources = [
     'src/main/c/jpy_module.c',
