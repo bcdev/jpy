19dd53e485cabcdab565035fff43fa5cccaa1e54
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-07-15 17:34:06 +0200

    Fixed a problem where jpy crashes with unicode arguments (issue #42)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 __author__ = "Norman Fomferra, Brockmann Consult GmbH"
 __copyright__ = "Copyright (C) 2014 Brockmann Consult GmbH"
 __license__ = "GPL v3"
-__version__ = "0.7.4-SNAPSHOT"
+__version__ = "0.7.4"
 
 sources = [
     'src/main/c/jpy_module.c',
