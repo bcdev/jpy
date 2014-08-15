7a3c28d469746df4a1421bbb86efb627e3f2cef2
forman <norman.fomferra@brockmann-consult.de>
2014-08-15 19:11:30 +0200

    Python extensions loaded from Java can't see Python symbols (Linux) #38

--- pysobug/setup.py
+++ pysobug/setup.py
@@ -3,12 +3,5 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-ext = Extension('mypymod',
-                sources=['mypymod.c'],
-                #libraries=['python3.4m'],
-)
-
-setup(name='mypymod', ext_modules=[ext])
-
-
-
+extension = Extension('mypymod', sources=['mypymod.c'], libraries=['python3.4m'])
+setup(name='mypymod', ext_modules=[extension])
