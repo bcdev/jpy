17fda2fbb1b61e4d545d7660455ca60b1cc91ba7
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2014-08-25 16:52:20 +0200

    Using PYTHONHOME

--- winbuild.cmd
+++ winbuild.cmd
@@ -19,22 +19,22 @@ IF NOT EXIST "%JDK64_HOME%" GOTO Build_32
 SET JAVA_HOME=%JDK64_HOME%
 CALL "%SDK_SETENV%" /x64 /release
 
-SET PY_HOME=%PY27_64%
-IF NOT EXIST "%PY_HOME%" GOTO Build_PY33_64
-ECHO Starting build using "%PY_HOME%" and "%JAVA_HOME%"
-"%PY_HOME%\python.exe" setup.py --maven build
+SET PYTHONHOME=%PY27_64%
+IF NOT EXIST "%PYTHONHOME%" GOTO Build_PY33_64
+ECHO Starting build using "%PYTHONHOME%" and "%JAVA_HOME%"
+"%PYTHONHOME%\python.exe" setup.py --maven build
 
 :Build_PY33_64
-SET PY_HOME=%PY33_64%
-IF NOT EXIST "%PY_HOME%" GOTO Build_PY34_64
-ECHO Starting build using "%PY_HOME%" and "%JAVA_HOME%"
-"%PY_HOME%\python.exe" setup.py --maven build
+SET PYTHONHOME=%PY33_64%
+IF NOT EXIST "%PYTHONHOME%" GOTO Build_PY34_64
+ECHO Starting build using "%PYTHONHOME%" and "%JAVA_HOME%"
+"%PYTHONHOME%\python.exe" setup.py --maven build
 
 :Build_PY34_64
-SET PY_HOME=%PY34_64%
-IF NOT EXIST "%PY_HOME%" GOTO Build_32
-ECHO Starting build using "%PY_HOME%" and "%JAVA_HOME%"
-"%PY_HOME%\python.exe" setup.py --maven build
+SET PYTHONHOME=%PY34_64%
+IF NOT EXIST "%PYTHONHOME%" GOTO Build_32
+ECHO Starting build using "%PYTHONHOME%" and "%JAVA_HOME%"
+"%PYTHONHOME%\python.exe" setup.py --maven build
 
 
 :Build_32
@@ -43,22 +43,22 @@ IF NOT EXIST "%JDK32_HOME%" GOTO Build_End
 SET JAVA_HOME=%JDK32_HOME%
 CALL "%SDK_SETENV%" /x86 /release
 
-SET PY_HOME=%PY27_32%
-IF NOT EXIST "%PY_HOME%" GOTO Build_PY33_32
-ECHO Starting build using "%PY_HOME%" and "%JAVA_HOME%"
-"%PY_HOME%\python.exe" setup.py --maven build
+SET PYTHONHOME=%PY27_32%
+IF NOT EXIST "%PYTHONHOME%" GOTO Build_PY33_32
+ECHO Starting build using "%PYTHONHOME%" and "%JAVA_HOME%"
+"%PYTHONHOME%\python.exe" setup.py --maven build
 
 :Build_PY33_32
-SET PY_HOME=%PY33_32%
-IF NOT EXIST "%PY_HOME%" GOTO Build_PY34_32
-ECHO Starting build using "%PY_HOME%" and "%JAVA_HOME%"
-"%PY_HOME%\python.exe" setup.py --maven build
+SET PYTHONHOME=%PY33_32%
+IF NOT EXIST "%PYTHONHOME%" GOTO Build_PY34_32
+ECHO Starting build using "%PYTHONHOME%" and "%JAVA_HOME%"
+"%PYTHONHOME%\python.exe" setup.py --maven build
 
 :Build_PY34_32
-SET PY_HOME=%PY34_32%
-IF NOT EXIST "%PY_HOME%" GOTO Build_End
-ECHO Starting build using "%PY_HOME%" and "%JAVA_HOME%"
-"%PY_HOME%\python.exe" setup.py --maven build
+SET PYTHONHOME=%PY34_32%
+IF NOT EXIST "%PYTHONHOME%" GOTO Build_End
+ECHO Starting build using "%PYTHONHOME%" and "%JAVA_HOME%"
+"%PYTHONHOME%\python.exe" setup.py --maven build
 
 
 :Build_End
