8e6c34f148d2058ce165ad91f284b413e784fad9
forman <norman.fomferra@brockmann-consult.de>
2014-08-25 17:42:04 +0200

    Fixed type incompatibility of py 2.7 and 3.4

--- src/main/c/jni/org_jpy_PyLib.c
+++ src/main/c/jni/org_jpy_PyLib.c
@@ -127,12 +127,21 @@ JNIEXPORT jboolean JNICALL Java_org_jpy_PyLib_startPython0
 
         if (JPy_DiagFlags != 0) {
             printf("PyLib_startPython: after Py_Initialize():\n");
+#if defined(JPY_COMPAT_33P)
+            printf("  Py_GetProgramName()     = \"%ls\"\n", Py_GetProgramName());
+            printf("  Py_GetPrefix()          = \"%ls\"\n", Py_GetPrefix());
+            printf("  Py_GetExecPrefix()      = \"%ls\"\n", Py_GetExecPrefix());
+            printf("  Py_GetProgramFullPath() = \"%ls\"\n", Py_GetProgramFullPath());
+            printf("  Py_GetPath()            = \"%ls\"\n", Py_GetPath());
+            printf("  Py_GetPythonHome()      = \"%ls\"\n", Py_GetPythonHome());
+#elif defined(JPY_COMPAT_27)
             printf("  Py_GetProgramName()     = \"%s\"\n", Py_GetProgramName());
             printf("  Py_GetPrefix()          = \"%s\"\n", Py_GetPrefix());
             printf("  Py_GetExecPrefix()      = \"%s\"\n", Py_GetExecPrefix());
             printf("  Py_GetProgramFullPath() = \"%s\"\n", Py_GetProgramFullPath());
-            printf("  Py_GetPythonHome()      = \"%s\"\n", Py_GetPythonHome());
             printf("  Py_GetPath()            = \"%s\"\n", Py_GetPath());
+            printf("  Py_GetPythonHome()      = \"%s\"\n", Py_GetPythonHome());
+#endif
             printf("  Py_GetVersion()         = \"%s\"\n", Py_GetVersion());
             printf("  Py_GetPlatform()        = \"%s\"\n", Py_GetPlatform());
             printf("  Py_GetCompiler()        = \"%s\"\n", Py_GetCompiler());
