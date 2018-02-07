b8fef51d5aea1335ed5b3380faed4aa81f7183d6
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2018-02-07 17:05:57 +0100

    another try...

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -1,58 +1,38 @@
 #!/bin/bash
 
-#if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-#
-#    # Install some custom requirements on OS X
-#    # e.g. brew install pyenv-virtualenv
-#    # See https://gist.github.com/Bouke/11261620
-#
-#    brew update
-#    brew install pyenv
-#    brew install pyenv-virtualenv
-#    eval "$(pyenv init -)"
-#    eval "$(pyenv virtualenv-init -)"
-#
-#    if [[ $TOXENV == '2.7' ]]; then
-#        pyenv virtualenv 2.7 jpy-venv
-#    elif [[ $TOXENV == '3.4' ]]; then
-#        pyenv virtualenv 3.4 jpy-venv
-#    elif [[ $TOXENV == '3.5' ]]; then
-#        pyenv virtualenv 3.5 jpy-venv
-#    elif [[ $TOXENV == '3.6' ]]; then
-#        pyenv virtualenv 3.6 jpy-venv
-#    fi
-#
-#else
-#    # Install pyenv
-#    # See https://github.com/pyenv/pyenv
-#    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
-#    export PYENV_ROOT="$HOME/.pyenv"
-#    export PATH="$PYENV_ROOT/bin:$PATH"
-#    eval "$(pyenv init -)"
-#
-#    # Install pyenv virtualenv plugin
-#    # See https://github.com/pyenv/pyenv-virtualenv
-#    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-#    eval "$(pyenv virtualenv-init -)"
-#
-#    # Create virtualenv from current Python
-#    pyenv virtualenv jpy-venv
-#fi
-#
-#pyenv activate jpy-venv
-#pip install wheel
-
-set -e
-set -x
-
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    hash -r
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+    # See https://gist.github.com/Bouke/11261620
+
+    brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
     eval "$(pyenv init -)"
-    hash -r
-    pyenv install --list
-    pyenv install -s $TOXENV
-    pip install wheel
+    eval "$(pyenv virtualenv-init -)"
+
+    pyenv virtualenv $TOXENV jpy-venv
+
+else
+    # Install pyenv
+    # See https://github.com/pyenv/pyenv
+    git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+
+    # Install pyenv virtualenv plugin
+    # See https://github.com/pyenv/pyenv-virtualenv
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+    eval "$(pyenv virtualenv-init -)"
+
+    # Create virtualenv from current Python
+    pyenv virtualenv jpy-venv
 fi
+
+pyenv activate jpy-venv
+pip install wheel
 
 
 
