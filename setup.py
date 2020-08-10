921c405486413f923d8fcc1a6efa442cf76daa3e
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2020-08-10 16:52:57 +0200

    Mac build: install wheel in pyenv

--- setup.py
+++ setup.py
@@ -357,4 +357,6 @@ setup(name='jpy',
 
                    # Specify the Python versions you support here. In particular, ensure
                    # that you indicate whether you support Python 2, Python 3 or both.
-                   'Programming Language :: Python :: 3.8'])
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.8'
+                   ])
